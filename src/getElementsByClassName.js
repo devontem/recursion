456475baf26dd119d7321804d10058b1780cfcf9
Devonte M <devontem@live.com>
2015-11-20 21:03:37 -0800

    completed stringify, and getElements (test still failing though)

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,32 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-){
-  // your code here
-};
+var getElementsByClassName = function(className){
+
+	var nodes = document.body.childNodes;
+	var collection = [];
+
+	// function to search through an array of nodes
+	var search = function(arr){
+		for (var i = 0 ; i < arr.length; i++){
+
+			// checks if node has children, then recursively checks children
+			if (arr[i].hasChildNodes()){
+				search(arr[i].childNodes);
+
+			}
+
+			//checks if node has the desired class name
+			if (arr[i].attributes != undefined && arr[i].attributes.class != undefined){
+				if (arr[i].attributes.class.value.indexOf(className) >= 0){
+					collection.push(arr[i]);
+				}
+			}
+		}
+	};
+
+	search(nodes);
+	return collection;
+}
+
+ 
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,6 +3,43 @@
 
 // but you don't so you're going to write it from scratch:
 
-var stringifyJSON = function(obj) {
-  // your code goes here
-};
+var stringifyJSON = function(obj){
+	// if null or undefined, exit function	
+	if (obj === null){
+		return String(null);
+	} else if (obj === undefined){
+		return '{}'
+	}
+	// testing for each primitive types
+	else if (obj.constructor === String){
+		return '"' + obj + '"';
+	}
+	else if (obj.constructor === Array){
+		var arr = [];
+		obj.forEach(function(val){
+			arr.push(stringifyJSON(val));
+		});
+		return '['+ arr.join(',') + ']';
+	} 
+	else if (obj.constructor === Boolean || obj.constructor === Number){
+		return String(obj);
+	}
+	// testing for objects
+	else if (obj.constructor === Object){
+		var arr = [];
+		for (var key in obj){
+
+			// returns empty object if value is a function or undefined
+			if (obj[key] != null && key != null){
+				if (obj[key].constructor === Function || obj[key] === undefined){
+					return '{}';
+				}
+			}
+
+			arr.push(stringifyJSON(key) + ':' + stringifyJSON(obj[key]));
+		}
+		return '{' + arr.join(",") + '}'
+	}
+}
+
+    
